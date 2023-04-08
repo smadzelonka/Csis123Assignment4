eeb131b77feff53da2ca33a03634c90b2eeb02f8
Sean Madzelonka <smadzelonka@gmail.com>
2023-04-07 22:06:29 -0500

    Initial Commit

---
+++ AssignmentFour/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  AssignmentFour
+//
+//  Created by Sean Madzelonka on 4/7/23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
